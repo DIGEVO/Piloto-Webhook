12a924c73f712ecf49790bf8b76160f1cb00592c
Rigoberto Leander Salgado Reyes <rlsalgado2006@@gmail.com>
2017-09-14 18:30:51 -0500

    testing

--- directline.js
+++ directline.js
@@ -2,7 +2,6 @@
 
 const Swagger = require('swagger-client');
 const rp = require('request-promise');
-const NodeCache = require('node-cache');
 const cache = new NodeCache({ stdTTL: 1800 });
 
 require('dotenv').config();
